cff76648634eb41f677a4f8147d253a03062ee80
Naoise Ryan <c00281427@itcarlow.ie>
2024-04-21 14:26:37 +0100

    Added links to nav bar and made header return to homepage when clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,6 @@
+function redirectToHomepage() {
+    document.location.href = "index.html";
+}
 
-
+//Event listensers
+document.getElementsByTagName("header")[0].addEventListener("click", redirectToHomepage); 
