d7fa8180f4f25784bedb2671dc5397486606e5e0
Naoise Ryan <c00281427@setu.ie>
2024-04-22 10:27:14 +0100

    Added hamburger menu and redid header css to fit hamburger

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,5 +2,17 @@ function redirectToHomepage() {
     document.location.href = "index.html";
 }
 
+function hamburgerMenu() {
+    const hamburger = document.getElementsByTagName("h1")[0];
+    if (hamburger.style.display != "none"){
+        alert("open hamburger")
+    }
+    else {
+        alert("close hamburger")
+    }
+}
+
 //Event listensers
-document.getElementsByTagName("header")[0].addEventListener("click", redirectToHomepage); 
+document.getElementsByTagName("h1")[0].addEventListener("click", redirectToHomepage); 
+
+document.getElementsByClassName("hamburger")[0].addEventListener("click", hamburgerMenu); 
